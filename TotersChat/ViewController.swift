a396494d9147148420eed19ceb8a387a42779f6d
Husam <husam.dev@gmail.com>
2020-08-11 20:07:45 +0300

    Init

---
+++ TotersChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TotersChat
+//
+//  Created by Husam Dayya on 8/11/20.
+//  Copyright © 2020 Husam Dayya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
