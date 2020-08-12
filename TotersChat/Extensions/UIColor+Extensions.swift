5ea8ac591c337cf5f452e4d4bce0b0969a148411
Husam <husam.dev@gmail.com>
2020-08-12 18:28:41 +0300

    uicolor extensions

---
+++ TotersChat/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extensions.swift
+//  TotersChat
+//
+//  Created by Husam Dayya on 8/12/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let myBlackColor = UIColor(red: 5/255, green: 13/255, blue: 19/255, alpha: 1)
+}
