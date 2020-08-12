545bb409190b418beeb4f5c4788ae47257ee5d3d
Husam <husam.dev@gmail.com>
2020-08-12 17:59:54 +0300

    added contact struct

---
+++ TotersChat/Models/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  TotersChat
+//
+//  Created by Husam Dayya on 8/12/20.
+//
+
+import Foundation
+
+struct Contact {
+    let id: String
+    let name: String
+    let image: String
+}
