d0bf1ca32b464d0aed1739d517730941019725a8
Husam <husam.dev@gmail.com>
2020-08-13 19:36:58 +0300

    position and color message cell

--- TotersChat/Screens/Chat/MessageTableViewCell.swift
+++ TotersChat/Screens/Chat/MessageTableViewCell.swift
@@ -13,7 +13,7 @@ class MessageTableViewCell: UITableViewCell {
         let v = UIView()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.clipsToBounds = true
-        v.layer.cornerRadius = 5
+        v.layer.cornerRadius = 20
         v.backgroundColor = .mylightBlack
         return v
     }()
@@ -30,10 +30,17 @@ class MessageTableViewCell: UITableViewCell {
         return t
     }()
     
+    var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.clipsToBounds = true
+        iv.backgroundColor = .darkGray
+        iv.layer.cornerRadius = 15
+        return iv
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        setupUI()
     }
     
     func setupUI() {
@@ -46,10 +53,37 @@ class MessageTableViewCell: UITableViewCell {
         addConstraints()
     }
     
+    fileprivate func addProfileImageView() {
+        contentView.addSubview(profileImageView)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            profileImageView.widthAnchor.constraint(equalToConstant: 30),
+            profileImageView.heightAnchor.constraint(equalToConstant: 30),
+            profileImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5)
+        ])
+        
+        contentView.layoutIfNeeded()
+    }
+    
+    func updateCell(isSend: Bool) {
+        setupUI()
+        
+        if isSend {
+            bubble.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
+            bubble.backgroundColor = .myGreen
+        } else {
+            bubble.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 40).isActive = true
+            bubble.backgroundColor = .mylightBlack
+            addProfileImageView()
+        }
+        
+        contentView.layoutIfNeeded()
+    }
+    
     func addConstraints() {
         let bubbleWidth = UIScreen.main.bounds.width * 0.65
         NSLayoutConstraint.activate([
-            bubble.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 30),
             bubble.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
             bubble.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
             bubble.widthAnchor.constraint(lessThanOrEqualToConstant: bubbleWidth),
@@ -61,6 +95,13 @@ class MessageTableViewCell: UITableViewCell {
         ])
         
         contentView.layoutIfNeeded()
+    }
+    
+    override func prepareForReuse() {
+        bubble.backgroundColor = nil
+        profileImageView.removeFromSuperview()
+        textView.removeFromSuperview()
+        bubble.removeFromSuperview()
     }
 
     required init?(coder aDecoder: NSCoder) {
