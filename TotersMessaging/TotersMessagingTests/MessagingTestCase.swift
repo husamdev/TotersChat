eb05114166eda3cadaf7a3688c9187feef3f7632
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 <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>
2022-01-24 21:57:18 +0200

    Refactored making SUT into factory function

--- TotersMessaging/TotersMessagingTests/MessagingTestCase.swift
+++ TotersMessaging/TotersMessagingTests/MessagingTestCase.swift
@@ -10,13 +10,19 @@ import XCTest
 class MessagingTestCase: XCTestCase {
 
     func test_init_doesNotSendMessagesOnInit() {
-        let client = MessagingClientSpy()
-        _ = MessagingService(client: client)
+        let (client, _) = makeSUT()
         
         XCTAssertEqual(client.sendMessageCallCount, 0)
     }
     
     // MARK: - Helpers
+    private func makeSUT() -> (client: MessagingClientSpy, service: MessagingService) {
+        let client = MessagingClientSpy()
+        let service = MessagingService(client: client)
+        
+        return (client, service)
+    }
+    
     private class MessagingClientSpy {
         var sendMessageCallCount = 0
     }
