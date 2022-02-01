2089233036504a2cd0a11f521bde0dfccd8e3a49
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 <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>
2022-02-02 00:13:13 +0200

    Extracted extensions to separate file in order to reuse them for other implementation

--- TotersMessaging/TotersMessagingTests/Persistence/CodableMessagesStoreTests.swift
+++ TotersMessaging/TotersMessagingTests/Persistence/CodableMessagesStoreTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import TotersMessaging
 
-class CodableMessagesStoreTests: XCTestCase {
+class CodableMessagesStoreTests: XCTestCase, MessageStoreSpecs {
     
     override func setUp() {
         super.setUp()
@@ -160,46 +160,6 @@ class CodableMessagesStoreTests: XCTestCase {
     }
     
     // MARK: - Helpers
-    private func expect(_ sut: MessageStore, toCompleteWith expectedResult: Result<[LocalMessage], Error>, whenContacting contact: Contact, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for completion")
-        
-        sut.retrieve(contact: contact.toLocal()) { recievedResult in
-            switch (recievedResult, expectedResult) {
-            case let (.success(recievedMessages), .success(expectedMessages)):
-                XCTAssertEqual(recievedMessages, expectedMessages, file: file, line: line)
-                
-            case (.failure, .failure):
-                break
-                
-            default:
-                XCTFail("Expected \(expectedResult) got \(recievedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func expect(_ sut: MessageStore, toRetrieveTwice expectedResult: Result<[LocalMessage], Error>, whenContacting contact: Contact) {
-        expect(sut, toCompleteWith: expectedResult, whenContacting: contact)
-        expect(sut, toCompleteWith: expectedResult, whenContacting: contact)
-    }
-    
-    @discardableResult
-    private func insert(_ sut: MessageStore, _ message: LocalMessage, file: StaticString = #file, line: UInt = #line) -> Error? {
-        let exp = expectation(description: "Wait for completion")
-        
-        var capturedError: Error?
-        sut.insert(message) { insertionError in
-            capturedError = insertionError
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-        
-        return capturedError
-    }
-    
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> MessageStore {
         let store = CodableMessagesStore(storeURL: storeURL ?? makeTestStoreURL())
         let sut = BackgroundQueueMessagesStore(decoratee: store)
